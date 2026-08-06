2f36031fe6309cedd2aae326c00d93b305ef54b0
dragon1842 <thedragon1842@gmail.com>
2026-08-06 08:01:14 +0100

    model update, planning migration back to openai for streamlining accounts and api keys

--- cogs/ai_generation.py
+++ cogs/ai_generation.py
@@ -14,12 +14,15 @@ load_dotenv()
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 TAVILY_API_KEY =  os.getenv("TAVILY_API_KEY")
-MODEL = "minimax/minimax-m3"
+MODEL = "openai/gpt-5.6-sol"
 
 generation_model = ChatOpenAI(
     model=MODEL,
     base_url="https://openrouter.ai/api/v1",
-    api_key=OPENROUTER_API_KEY
+    api_key=OPENROUTER_API_KEY,
+    openrouter_provider=
+        {"order" : ["azure"],
+         "allow_fallbacks" : True},
     )
 
 web_search = TavilySearch(
